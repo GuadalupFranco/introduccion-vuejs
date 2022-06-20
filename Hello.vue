623be504d690161ddbd520a9a9c47c4db5f8e1d2
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2022-06-20 08:44:43 -0500

    22-progressive-vue

---
+++ Hello.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit magnam voluptatem sunt debitis dolorem officia obcaecati accusantium sapiente? Rem alias quo illo eum, repellat numquam itaque delectus aperiam magnam quod?</p>
+        <div> {{ Hi }} </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            text: "Hi"
+        };
+    },
+    setup() {
+        
+    },
+}
+</script>
+
+<style>
+p {
+    color: red;
+}
+</style>
